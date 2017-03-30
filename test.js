1ffd1ab5f7c5e14471c390844c98c5e6d5745034
Alhadis <gardnerjohng@gmail.com>
2017-03-30 16:47:40 +1100

    Add GNU Troff's colour-scheme commands to table

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 "use strict";
 
 const fs = require("fs");
-const input = fs.readFileSync("drawing.ditroff").toString();
+const input = fs.readFileSync("simple.ditroff").toString();
 
 const {tokenise, list} = require("./tokenise-ditroff.js");
 list(tokenise(input));
