6026524200b5bd70afd642d889caa7e8aa67aa87
Alhadis <gardnerjohng@gmail.com>
2017-04-01 18:50:58 +1100

    Add partial support for TTY-formatted output
    Still being worked on:
    * Whitespace being incorrectly underlined
    * Absolute horizontal movements still buggy

--- test.js
+++ test.js
@@ -2,7 +2,18 @@
 "use strict";
 
 const fs = require("fs");
-const input = fs.readFileSync("./fixtures/simple.ditroff").toString();
+const input = fs.readFileSync("./fixtures/man.ditroff").toString();
 
-const {tokenise, list} = require("./lib/tokeniser.js");
-list(tokenise(input));
+const {tokenise, ttyToHTML} = require("./lib/tokeniser.js");
+const html = ttyToHTML(tokenise(input));
+console.log(htmlToTTY(html));
+
+function htmlToTTY(input){
+	return input
+		.replace(/^\n+/,   "")
+		.replace(/<b>/g,   "\x1B[1m")
+		.replace(/<u>/g,   "\x1B[4m")
+		.replace(/<\/b>/g, "\x1B[22m")
+		.replace(/<\/u>/g, "\x1B[24m")
+		.replace(/&#(\d+);/g, (_,c) => String.fromCharCode(c));
+}
