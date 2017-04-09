c939bbe043960b58f7d46356c1ae4044077e53be
Alhadis <gardnerjohng@gmail.com>
2017-04-09 22:54:43 +1000

    Rewrite parsing logic to use TextGrid class

--- test.js
+++ test.js
@@ -1,26 +1,9 @@
 #!/usr/local/bin/node --es_staging
 "use strict";
 
-const TextGrid = require("./lib/text-grid.js");
-let t = new TextGrid()
-	.write("Lorem ipsum ")
-	.write("dolor sit amet");
-console.log(t.toString());
-process.exit(0);
+const parseTTY = require("./lib/parse-tty.js");
 
-const fs = require("fs");
-const input = fs.readFileSync("./fixtures/tbl.ditroff").toString();
+const input = require("fs").readFileSync(__dirname + "/fixtures/tbl.ditroff").toString();
+const output = parseTTY(input);
 
-const {tokenise, ttyToHTML} = require("./lib/tokeniser.js");
-const html = ttyToHTML(tokenise(input));
-console.log(htmlToTTY(html));
-
-function htmlToTTY(input){
-	return input
-		.replace(/^\n+/,   "")
-		.replace(/<b>/g,   "\x1B[1m")
-		.replace(/<u>/g,   "\x1B[4m")
-		.replace(/<\/b>/g, "\x1B[22m")
-		.replace(/<\/u>/g, "\x1B[24m")
-		.replace(/&#(\d+);/g, (_,c) => String.fromCharCode(c));
-}
+console.log(output);
