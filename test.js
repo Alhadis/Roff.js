6572a68f839961fa915f804f14e25c719e38ad53
Alhadis <gardnerjohng@gmail.com>
2017-04-10 23:05:26 +1000

    Implement remainder of supported output commands

--- test.js
+++ test.js
@@ -4,6 +4,11 @@
 const parseTTY = require("./lib/parse-tty.js");
 
 const input = require("fs").readFileSync(__dirname + "/fixtures/tbl.ditroff").toString();
+
+const start = Date.now();
 const output = parseTTY(input);
+const end = Date.now();
 
 console.log(output);
+
+console.log(`Time: ${end - start}ms`);
