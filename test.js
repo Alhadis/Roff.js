1f3e1c66c8293eef70837406d36ee26184badcb0
Alhadis <gardnerjohng@gmail.com>
2017-05-15 04:04:24 +1000

    Add TTYCanvas support for diagonal line-drawing

--- test.js
+++ test.js
@@ -1,8 +1,43 @@
 #!/usr/bin/env node --es_staging
 "use strict";
 
-const parseTTY = require("./lib/render-tty.js");
-const input = require("fs").readFileSync(__dirname + "/fixtures/text-2.out").toString();
+const TTYCanvas = require("./lib/tty-canvas.js");
+let t;
 
-const output = parseTTY(input).trim();
-console.log(output);
+t = new TTYCanvas()
+	.drawLine(20, 0)
+	.drawLine(20, 20)
+	.up()
+	.drawLine(20, 0)
+	.drawLine(0, 2)
+	.drawLine(-2, 2)
+	.up()
+	.drawLine(-44, 0)
+	.drawLine(0, -3)
+	.drawLine(20, -20)
+	.drawLine(5)
+	.left()
+	.drawLine(0, 2)
+	.drawLine(-19, 19)
+	.drawLine(17, 0)
+	.drawLine(-25, -25)
+console.log(t.toString());
+
+t = new TTYCanvas()
+	.draw([
+		[20,  0],
+		[20,  20],
+		[20,  0],
+		[0,   2],
+		[-2,  2],
+		[-44, 0],
+		[0,  -3],
+		[20, -20],
+		[4,   0],
+		[0,   2],
+		[-20, 20],
+		[17,  0],
+		[-25, -25],
+	]);
+
+console.log(t.toString());
