47d4eda8cdc2c128091de5ce15d324b8da6d060f
Alhadis <gardnerjohng@gmail.com>
2017-07-01 03:46:19 +1000

    Refactor canvas-rendering logic to extend Postprocessor class

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 "use strict";
 
 module.exports = {
-	Postprocessor: require("./lib/tokeniser.js"),
-	TextGrid:      require("./lib/text-grid.js"),
-	HTMLTTY:       require("./lib/postproc/html-tty.js"),
-	TroffCanvas:   require("./lib/postproc/canvas.js"),
+	Postprocessor:  require("./lib/tokeniser.js"),
+	TextGrid:       require("./lib/text-grid.js"),
+	HTMLTTY:        require("./lib/postproc/html-tty.js"),
+	CanvasRenderer: require("./lib/postproc/canvas.js"),
 };
