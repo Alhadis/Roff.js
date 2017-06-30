6a332d5f5ea4757346e8bb042a567a8dfcc62463
Alhadis <gardnerjohng@gmail.com>
2017-06-30 19:03:06 +1000

    Add tests for box-drawing and track-kerning

--- test/1-html-tty.js
+++ test/1-html-tty.js
@@ -9,14 +9,31 @@ const read = (fixtureFile) =>
 	fs.readFileSync(join(__dirname, "fixtures", fixtureFile), "utf8");
 
 describe("HTMLTTY", () => {
-	const [tmplHeader, tmplFooter] = read("template.html").trim().split(/\n+/);
 	
-	for(const manpage of ["groff_char(7)", "perlre(1)"]){
-		it(`formats ${manpage} correctly`, () => {
-			const source   = read(manpage.replace(/\(|\)/g, ".") + "out");
-			const expected = read(manpage.replace(/\(|\)/g, ".") + "html");
-			const result   = [tmplHeader, htmlTTY.parse(source), tmplFooter, ""].join("\n");
-			expect(result).to.eql(expected);
+	describe("Basic procedures", () => {
+		it("prints text with track-kerning", () => {
+			const source   = read("text-tracking.out");
+			const expected = read("text-tracking.txt");
+			expect(htmlTTY.parse(source) + "\n").to.eql(expected);
 		});
-	}
+		
+		it("draws horizontal and vertical lines", () => {
+			const source   = read("boxes.out");
+			const expected = read("boxes.txt");
+			expect(htmlTTY.parse(source) + "\n").to.eql(expected);
+		});
+	});
+	
+	describe("Manpage formatting", () => {
+		const [tmplHeader, tmplFooter] = read("template.html").trim().split(/\n+/);
+		
+		for(const manpage of ["groff_char(7)", "perlre(1)"]){
+			it(`formats ${manpage} correctly`, () => {
+				const source   = read(manpage.replace(/\(|\)/g, ".") + "out");
+				const expected = read(manpage.replace(/\(|\)/g, ".") + "html");
+				const result   = [tmplHeader, htmlTTY.parse(source), tmplFooter, ""].join("\n");
+				expect(result).to.eql(expected);
+			});
+		}
+	});
 });
