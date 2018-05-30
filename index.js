e9faca06481c068916bb83cf004e548c0a737fe9
Alhadis <gardnerjohng@gmail.com>
2018-05-31 00:31:01 +1000

    Migrate manpage-specific logic from Atom package

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 "use strict";
 
+let shell = null;
+
 module.exports = {
-	Postprocessor:  require("./lib/tokeniser.js"),
-	TextGrid:       require("./lib/text-grid.js"),
-	HTMLTTY:        require("./lib/postproc/html-tty.js"),
-	CanvasRenderer: require("./lib/postproc/canvas.js"),
+	get Postprocessor()  {return require("./lib/postprocessor.js")},
+	get CanvasRenderer() {return require("./lib/postproc/canvas.js")},
+	get HTMLTTY()        {return require("./lib/postproc/html-tty.js")},
+	get TextGrid()       {return require("./lib/text-grid.js")},
+	get ManPager()       {return require("./lib/manpager.js")},
+	get TroffType()      {return require("./lib/troff-type.js")},
+	get TroffView()      {return require("./lib/troff-view.js")},
+	
+	// Expose a shared cache for external operations
+	get shell(){
+		return null === shell
+			? shell = new (require("./lib/shell-cache.js"))
+			: shell;
+	},
 };
