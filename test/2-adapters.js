d6ee8bc92c65a00fa1cef875db19601f2f611ba8
Alhadis <gardnerjohng@gmail.com>
2018-08-16 17:30:52 +1000

    Start work on more sustainable integration methods

---
+++ test/2-adapters.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const {expect} = require("chai");
+
+
+describe("Adapters", () => {
+	
+	describe("Utility functions", () => {
+		const utils = require("../lib/adapters/utils.js");
+
+		describe("parseManURL", () => {
+			
+		});
+
+		describe("resolveManRef", () => {
+			
+		});
+	});
+});
