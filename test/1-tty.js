aa657950360b2b09cefb4f87add5b64c8047497d
Alhadis <gardnerjohng@gmail.com>
2018-11-22 22:22:29 +1100

    Allow tests to run using global Chai installations

--- test/1-tty.js
+++ test/1-tty.js
@@ -4,7 +4,6 @@ const fs            = require("fs");
 const path          = require("path");
 const {exec}        = require("child_process");
 const {promisify}   = require("util");
-const {expect}      = require("chai");
 const {TTYRenderer} = require("../");
 const htmlTTY       = new TTYRenderer();
 
